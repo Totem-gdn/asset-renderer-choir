e23f3257a0e88839144bf6e493e991bc6bfc0bb2
illiadimension <ilya.nikulin@it-dimension.com>
2023-01-26 14:34:49 +0200

    add choir avatar

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # asset-renderer-choir
+
+
+
+
+## Resources
+
+> In every render we have resources for bind item or avatar for svg, its ai or psd files, which have parts and mask for render.
+
+
+
+## Logic
+> We have helpers/dna-parser.js file, where we have logic for getting json objects with params for every renderer, for avatar and items its object are differnt
+
+> In this file controllers/api/nft.js we getting params for image, its width, height, type, and id image and depend of type, we geting HBS file in folder 'layouts' and binding render
---
+++ controllers/api/nft.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const nftHelper = require('../../helpers/dna-parser')
+
+class NFTController {
+  async get (req, res, next) {
+    const { type, id } = req.params
+    const { width = 200, height = 200 } = req.query
+    if (!type || !id) {
+      res.status(404).json({ error: 'Wrong format' })
+    }
+    if (type) {
+      const nft = await nftHelper.get(type, id);
+      // nft.body_type = 'Fat';
+      // nft.body_strength = 'Muscular';
+      // nft.hair_styles = 'Afro'
+      nft['glow_color'] = nft.primary_color.replace(')', ', 0.5)').replace('rgb', 'rgba');
+
+      console.log('nft', nft);
+      res.setHeader('Content-Type', 'image/svg+xml');
+      res.render(`layouts/${type}.hbs`, {
+        layout: `${type}.hbs`,
+        ...nft,
+        width: width,
+        height: height
+      })
+    } else {
+      res.status(404).json({ error: 'File not found' })
+    }
+  }
+}
+
+module.exports = new NFTController()
